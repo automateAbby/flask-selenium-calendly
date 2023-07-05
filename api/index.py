4325c8fa148c9c8303ad530a3fb28f04cd5e6cfe
automateAbby <138645677+automateAbby@users.noreply.github.com>
2023-07-05 16:01:57 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,4 +1,3 @@
-import asyncio
 from flask import Flask
 from flask import request
 from selenium import webdriver
@@ -12,28 +11,20 @@ browser = webdriver.Chrome(options=chrome_options)
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return 'Hello, World!'
+def hello_world():
+    return 'Hello! If this api wont work, contact me at automate.abby@gmail.com'
 
-@app.route('/schedule/', methods=['POST'])
-async def automate():
+@app.route('/schedule/', methods = ['POST'])
+def automate():
     if request.method == 'POST':
         try:
             name = request.form.get('name')
             email = request.form.get('email')
             url = str(request.form.get('url'))
-
-            await asyncio.sleep(0)  # Allow other tasks to run (if any)
-
-            browser.get(url)
+            browser.get('https://calendly.com/automate-abby/meeting-appointment/2023-07-05T00:00:00Z')
             inputEl = browser.find_element_by_id('full_name_input').send_keys(name)
             inputEl = browser.find_element_by_id('email_input').send_keys(email)
             inputEl.submit()
-            await asyncio.sleep(0)  # Allow time for the submission to complete (if necessary)
             return format(browser.title)
         finally:
             browser.quit()
-
-if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(asyncio.ensure_future(app.run(debug=True)))
